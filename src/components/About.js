8b0caab76531e9fac5c8869060964736d5f3c79d
Joseph Cheng <jxc2008@nyu.edu>
2025-07-02 22:37:20 -0400

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+function About() {
+    return (
+      <section>
+        <h2>About Me</h2>
+        <p>I’m a [your profession/interest] who loves building things.</p>
+      </section>
+    );
+  }
+  export default About;
+  
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+      <header style={{ padding: '20px', background: '#282c34', color: 'white' }}>
+        <h1>Your Name</h1>
+      </header>
+    );
+  }
+  export default Header;
+  
